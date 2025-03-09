9b574298fee8ba942224ef490163532dc72e9f52
blbergo <bryan.l.bergo@gmail.com>
2025-03-08 17:21:33 -0800

    feat: pull supabase auth schema

---
+++ src/db/schema/videos.ts
@@ -0,0 +1,24 @@
+import { pgEnum, pgSchema, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const videos = pgSchema("videos");
+
+export const metadata = videos.table("metadata", {
+    id: uuid().primaryKey(),
+    // patient_id: uuid().references(patients),
+    filename: varchar({ length: 255 }).notNull(),
+    url: text().notNull(),
+    createdAt: timestamp().defaultNow(),
+    updatedAt: timestamp().defaultNow(),
+    deletedAt: timestamp()
+});
+
+export const video_status = pgEnum("video_status", ["pending", "processing", "processed", "failed"]);
+
+export const status = videos.table("status", {
+    id: uuid().primaryKey(),
+    video_id: uuid().references(() => metadata.id).notNull(),
+    status: video_status().notNull().default("pending"),
+    createdAt: timestamp().defaultNow(),
+    updatedAt: timestamp().defaultNow(),
+    deletedAt: timestamp()
+});
