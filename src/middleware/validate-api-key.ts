b1a8f3be2c07726610502de46f059031aff394b1
blbergo <bryan.l.bergo@gmail.com>
2025-03-08 20:25:35 -0800

    chore: tests

--- src/middleware/validate-api-key.ts
+++ src/middleware/validate-api-key.ts
@@ -9,19 +9,18 @@ import { retrieveApiKey } from "../services/auth";
  * @throws {HTTPException} With status 401 if Authorization header is missing or API key is invalid
  */
 export const validateApiKey = async (c: Context, next: Next) => {
-    const authHeader = c.req.header("Authorization");
+  const authHeader = c.req.header("Authorization");
 
-    if (!authHeader) {
-        throw new HTTPException(401, {message: "Unauthorized"});
-    }
+  if (!authHeader) {
+    throw new HTTPException(401, { message: "Unauthorized" });
+  }
 
-    const [_, apiKey] = authHeader.split("Bearer ");
+  const [_, apiKey] = authHeader.split("Bearer ");
+  const result = await retrieveApiKey(apiKey);
 
-    const result = await retrieveApiKey(apiKey);
+  if (!result) {
+    throw new HTTPException(401, { message: "Unauthorized" });
+  }
 
-    if (!result) {
-        throw new HTTPException(401, {message: "Unauthorized"});
-    }
-
-    next();
-}
+  next();
+};
