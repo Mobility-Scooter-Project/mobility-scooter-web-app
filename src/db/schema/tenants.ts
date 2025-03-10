8a63e5409cf406df5a0a5a9fd2792331ede7f23e
blbergo <bryan.l.bergo@gmail.com>
2025-03-09 17:50:36 -0700

    feat: tenant schema

---
+++ src/db/schema/tenants.ts
@@ -0,0 +1,22 @@
+import { pgSchema, uuid, text, timestamp } from "drizzle-orm/pg-core";
+
+export const tenants = pgSchema("tenants");
+
+export const metadata = tenants.table("metadata", {
+  id: uuid().primaryKey(),
+  name: text().notNull(),
+  createdAt: timestamp().defaultNow(),
+  updatedAt: timestamp().defaultNow(),
+  deletedAt: timestamp(),
+});
+
+export const units = tenants.table("units", {
+  id: uuid().primaryKey(),
+  tenantId: uuid()
+    .references(() => metadata.id)
+    .notNull(),
+  // adminUserId: uuid().references(() => users.id).notNull(),
+  createdAt: timestamp().defaultNow(),
+  updatedAt: timestamp().defaultNow(),
+  deletedAt: timestamp(),
+});
