6fccd54ca0e52872d72c58de49b4e35a31d31943
blbergo <bryan.l.bergo@gmail.com>
2025-03-10 10:10:57 -0700

    fix: refactor for RLS

---
+++ src/middleware/db.ts
@@ -0,0 +1,35 @@
+import { DATABASE_URL } from "@config/constants";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Context, Next } from "hono";
+import { Pool } from "pg";
+import * as auth from "../db/schema/auth";
+import * as videos from "../db/schema/videos";
+import * as tenants from "../db/schema/tenants";
+
+export const dbMiddleware = async (c: Context, next: Next) => {
+  const userId = c.get("userId");
+
+  const pool = new Pool({
+    connectionString: DATABASE_URL,
+  });
+
+  const db = drizzle({
+    client: pool,
+    casing: "snake_case",
+    schema: { ...auth, ...videos },
+  });
+
+  if (userId) {
+    await db.execute(`SET LOCAL app.user_id = ${userId}`);
+  }
+
+  await next();
+};
+
+// For typing only
+export const db = drizzle(DATABASE_URL, {
+  casing: "snake_case",
+  schema: { ...auth, ...videos, ...tenants },
+});
+
+export type DB = typeof db;
