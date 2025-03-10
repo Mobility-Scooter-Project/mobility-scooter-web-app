d5117871aa0d4153076bfe1c65da58983d20b5b6
blbergo <bryan.l.bergo@gmail.com>
2025-03-09 17:43:58 -0700

    fix: remove api key requirement for healthcheck

--- src/db/schema/auth.ts
+++ src/db/schema/auth.ts
@@ -1,15 +1,16 @@
 import { boolean, pgSchema, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
-
 export const auth = pgSchema("auth");
 
 export const apiKeys = auth.table("api_keys", {
-	id: uuid().primaryKey().notNull(),
-	encryptedKey: text().notNull(),
-	owner: text().notNull(),
-	// scopes: jsonb().notNull(),
-	isActive: boolean().default(true),
-	createdAt: timestamp().defaultNow(),
-	updatedAt: timestamp().defaultNow(),
-	deletedAt: timestamp()
+  id: uuid().primaryKey().notNull(),
+  encryptedKey: text().notNull(),
+  owner: text().notNull(),
+  // scopes: jsonb().notNull(),
+  isActive: boolean().default(true),
+  createdAt: timestamp().defaultNow(),
+  updatedAt: timestamp().defaultNow(),
+  deletedAt: timestamp(),
 });
+
+export const users = auth.table("users", {});
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
-import { validateApiKey } from "./middleware/validate-api-key";
 
-export const app = new Hono()
-  .use(logger())
-  .use(validateApiKey)
-  .get("/healthcheck", (c) => {
-    return new Response("OK", { status: 200 });
-  });
+export const app = new Hono().use(logger()).get("/healthcheck", (c) => {
+  return new Response("OK", { status: 200 });
+});
 
 serve(
   {
