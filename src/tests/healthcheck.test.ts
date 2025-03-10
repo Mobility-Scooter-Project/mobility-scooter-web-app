d02ac1a22cce496a1ab1ed7f4a9af4bc6706dc9c
blbergo <bryan.l.bergo@gmail.com>
2025-03-09 17:43:58 -0700

    fix: remove api key requirement for healthcheck

--- src/tests/healthcheck.test.ts
+++ src/tests/healthcheck.test.ts
@@ -5,19 +5,9 @@ const headers = {
   Authorization: `Bearer ${process.env.TESTING_API_KEY}`,
 };
 
-it("should return 401 Unauthorized", async () => {
-  const response = await testClient(app).healthcheck.$get();
-
-  expect(response.status).toBe(401);
-});
 
 it("should return 200 OK", async () => {
-  const response = await testClient(app).healthcheck.$get(
-    {},
-    {
-      headers,
-    }
-  );
+  const response = await testClient(app).healthcheck.$get();
 
   expect(response.status).toBe(200);
 });
