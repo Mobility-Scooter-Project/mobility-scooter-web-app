9c4119c0eadd305942adb465b98050e0f597165d
blbergo <bryan.l.bergo@gmail.com>
2025-04-26 13:23:18 -0700

    chore: tsconfigs

--- apps/api/src/integrations/queue.ts
+++ apps/api/src/integrations/queue.ts
@@ -1,5 +1,5 @@
 import { QUEUE_URL } from "@src/config/constants"
-import { Queue } from "../../../../packages/shared/integrations/queue"
+import { Queue } from "@shared/integrations/queue"
 import { HTTPError } from "@src/lib/errors"
 
 export const queue = new Queue(QUEUE_URL, new HTTPError(500, "Failed to connect to RabbitMQ"));
