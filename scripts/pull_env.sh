e6c4b266e15ec3b971e0f33950d5d68f75a9d793
Bryan Bergo <93395362+blbergo@users.noreply.github.com>
2025-08-29 16:20:56 -0700

    Bryan/video worker chart (#33)
    * feat: initial worker
    
    * fix: switch target branch
    
    * fix: add more memory to worker
    
    * fix: update node + python versions, remove gpu from chart
    
    * fix: update infisical project id
    
    * fix: update env script

--- scripts/pull_env.sh
+++ scripts/pull_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 infisical login --domain https://infisical.cis240470.projects.jetstream-cloud.org
-infisical secrets --projectId e3994b3d-5080-48f1-bb0f-c37f3b9793e9 --env dev --plain --tags api > apps/api/.env
+infisical secrets --projectId 7ea0e637-93ef-4c3f-b1e3-4563ed037d64 --env dev --plain > apps/api/.env
 
 echo "Pulled latest .env from Infisical"
