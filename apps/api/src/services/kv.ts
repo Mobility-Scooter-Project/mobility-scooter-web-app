1e16c99f4ed56dc81e545d5dd88104b7d8a0a65d
blbergo <bryan.l.bergo@gmail.com>
2025-09-11 16:24:58 -0700

    fix: pin python version

--- apps/api/src/services/kv.ts
+++ apps/api/src/services/kv.ts
@@ -6,23 +6,7 @@ import { Redis } from "ioredis";
 @injectable()
 export class KVService {
     public static async build(): Promise<Redis> {
-        const redis = new Promise<Redis>((resolve, reject) => {
-            try {
-                const redis = new Redis(KV_URL);
-                redis.on("error", (err) => {
-                    console.error("Redis connection error:", err);
-                    reject(err);
-                });
-                redis.on("connect", () => {
-                    console.log("Connected to Redis");
-                    resolve(redis);
-                });
-            } catch (error) {
-                console.error("Failed to create Redis connection:", error);
-                reject(error);
-            }
-        });
-
-        return await redis;
+        const redis = new Redis(KV_URL, { lazyConnect: true });
+        return redis;
     }
 }
