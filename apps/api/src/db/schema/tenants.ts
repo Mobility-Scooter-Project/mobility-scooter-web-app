dd1dbc58b7ce332fdd2173f6ee018b8b11106214
Bryan <bryan.l.bergo@gmail.com>
2025-09-22 13:57:07 -0700

    feat: db service

---
+++ apps/api/src/db/schema/tenants.ts
@@ -0,0 +1,22 @@
+import { pgSchema, uuid, text, timestamp } from "drizzle-orm/pg-core";
+
+export const tenants = pgSchema("tenants");
+
+export const metadata = tenants.table("metadata", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: text().notNull(),
+  createdAt: timestamp().defaultNow(),
+  updatedAt: timestamp().defaultNow(),
+  deletedAt: timestamp(),
+});
+
+export const units = tenants.table("units", {
+  id: uuid().primaryKey().defaultRandom(),
+  tenantId: uuid()
+    .references(() => metadata.id)
+    .notNull(),
+  adminUserId: uuid(), // cannot use a circular reference here
+  createdAt: timestamp().defaultNow(),
+  updatedAt: timestamp().defaultNow(),
+  deletedAt: timestamp(),
+});
