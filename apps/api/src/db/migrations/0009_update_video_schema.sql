ae5ad0ae767232fa5160f0515eeb02c5b7873714
Bryan <bryan.l.bergo@gmail.com>
2025-09-22 13:38:25 -0700

    feat: nest setup

--- apps/api/src/db/migrations/0009_update_video_schema.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "videos"."status" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "videos"."metadata" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "videos"."metadata" DROP COLUMN "url";
--- apps/api/src/db/migrations/0012_strong_tyger_tiger.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "videos"."tasks" ADD COLUMN "task_id" integer DEFAULT 1 NOT NULL;--> statement-breakpoint
-ALTER TABLE "videos"."tasks" ADD COLUMN "created_at" timestamp DEFAULT now();--> statement-breakpoint
-ALTER TABLE "videos"."tasks" ADD COLUMN "updated_at" timestamp DEFAULT now();--> statement-breakpoint
-ALTER TABLE "videos"."tasks" ADD COLUMN "deleted_at" timestamp;
