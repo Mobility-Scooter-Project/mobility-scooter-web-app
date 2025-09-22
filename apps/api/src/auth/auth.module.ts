a6229220f02365d8bfbb6c2889c65c3d52cb46b7
Bryan <bryan.l.bergo@gmail.com>
2025-09-22 16:01:01 -0700

    feat: auth service

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppConfig } from 'src/config';
+import { InfraModule } from 'src/infra/infra.module';
 
-@Module({})
-export class AuthModule {}
+@Module({
+  providers: [AuthService],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService<AppConfig>) => ({
+        secret: configService.get("jwtSecret"),
+      }),
+      inject: [ConfigService],
+    }),
+    InfraModule
+  ]
+})
+export class AuthModule { }
