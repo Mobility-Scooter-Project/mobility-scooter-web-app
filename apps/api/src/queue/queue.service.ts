6e59ce6489b77454d9211518777abe47808a8794
Bryan <bryan.l.bergo@gmail.com>
2025-09-22 14:17:26 -0700

    feat: queue service

---
+++ apps/api/src/queue/queue.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Kafka, Producer } from 'kafkajs';
+import { AppConfig } from 'src/config';
+
+@Injectable()
+export class QueueService {
+    private client: Kafka;
+    private producer: Producer;
+
+    private constructor(private readonly configService: ConfigService<AppConfig>) {
+        this.client = new Kafka({
+            clientId: 'api',
+            brokers: [this.configService.get("broker").url],
+        });
+        this.producer = this.client.producer();
+    }
+
+    public static async build(configService: ConfigService<AppConfig>): Promise<QueueService> {
+        const queue = new QueueService(configService);
+        await queue.producer.connect();
+        return queue;
+    }
+}
