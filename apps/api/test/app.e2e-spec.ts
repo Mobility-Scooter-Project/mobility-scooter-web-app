d23aa5f40dad3faa2279c083ea63a0e145afe456
Bryan <bryan.l.bergo@gmail.com>
2025-09-22 13:40:49 -0700

    fix: use main instead of sub-app

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -1,14 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { ApiModule } from './../src/api.module';
+import { App } from 'supertest/types';
+import { AppModule } from './../src/app.module';
 
-describe('ApiController (e2e)', () => {
-  let app: INestApplication;
+describe('AppController (e2e)', () => {
+  let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [ApiModule],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
