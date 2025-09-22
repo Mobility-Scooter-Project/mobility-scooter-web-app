ededfb6eafb1026a9c46d200c39fafb6ebe98216
Bryan <bryan.l.bergo@gmail.com>
2025-09-22 15:27:10 -0700

    fix: refactor infra

--- apps/api/src/kv/kv.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { KvService } from './kv.service';
-
-@Module({
-    providers: [KvService],
-    exports: [KvService],
-})
-export class KvModule { }
