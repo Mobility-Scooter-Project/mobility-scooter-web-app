0b68e7ae14bd3a289c33d343bc8866803a8d4386
Bryan <bryan.l.bergo@gmail.com>
2025-09-22 18:32:13 -0700

    feat: sprint 1 (0 was prev)

---
+++ apps/api/src/orgs/orgs.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Patch, Post, Put } from '@nestjs/common';
+
+@Controller('organizations')
+export class OrgsController {
+    @Post('applications')
+    async createOrgApplication() {
+        // TODO
+    }
+
+    @Patch('applications/:applicationId')
+    async editOrgApplication() {
+        // TODO
+    }
+
+    @Post('applications/:applicationId')
+    async submitOrgApplication() {
+        // TODO
+    }
+
+    @Put('organizations/:orgId')
+    async updateOrg() {
+        // TODO
+    }
+
+    @Post('organizations/:orgId')
+    async completeOnboarding() {
+        // TODO
+    }
+}
