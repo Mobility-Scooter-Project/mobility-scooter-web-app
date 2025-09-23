9f979faf3314962421ecb42111268049e3b51ce6
Bryan <bryan.l.bergo@gmail.com>
2025-09-22 19:08:40 -0700

    feat: sprint 3

--- apps/api/src/units/units.module.ts
+++ apps/api/src/units/units.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UnitsController } from './units.controller';
 import { SessionsController } from './sessions/sessions.controller';
 import { InvitesController } from './invites/invites.controller';
+import { AssignmentsController } from './assignments/assignments.controller';
 
 @Module({
-  controllers: [UnitsController, SessionsController, InvitesController]
+  controllers: [UnitsController, SessionsController, InvitesController, AssignmentsController]
 })
 export class UnitsModule {}
