61749f3aee2512abd0ffd38ebccbfd4c0d179c98
Bryan <bryan.l.bergo@gmail.com>
2025-09-22 19:01:40 -0700

    feat: sprint 2

---
+++ apps/api/src/units/invites/invites.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller('units/:unitId/invites')
+export class InvitesController {
+    @Post()
+    async createInvite() {
+        // TODO
+    }
+
+    @Post(':inviteToken')
+    async acceptInvite() {
+        // TODO
+    }
+}
