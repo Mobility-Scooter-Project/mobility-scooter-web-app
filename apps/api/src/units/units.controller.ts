a5a6c798b958c20ec2330ec6664666587de7a06e
Bryan <bryan.l.bergo@gmail.com>
2025-09-22 18:23:01 -0700

    feat: sprint 1

---
+++ apps/api/src/units/units.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param, Patch } from '@nestjs/common';
+
+@Controller('units')
+export class UnitsController {
+    @Get(':unitId/users')
+    async getUsers(@Param('unitId') unitId: string) {
+        // TODO
+    }
+
+    @Patch(':unitId/:userId')
+    async updateUserProfile(
+        @Param('unitId') unitId: string,
+        @Param('userId') userId: string
+    ) {
+        // TODO
+    }
+}
