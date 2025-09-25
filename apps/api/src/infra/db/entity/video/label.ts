b2393bce949ec3d164f7dded360eae40da46348d
Bryan <bryan.l.bergo@gmail.com>
2025-09-25 14:36:32 -0700

    feat: assignment migration

--- apps/api/src/infra/db/entity/video/label.ts
+++ apps/api/src/infra/db/entity/video/label.ts
@@ -2,6 +2,7 @@ import { SCHEMAS } from "@src/config/schemas";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { CreateUpdateFields } from "../shared";
 import { Video } from "./video";
+import { Assignment } from "../unit/assignment";
 
 @Entity({ schema: SCHEMAS.VIDEOS })
 export class VideoLabel {
@@ -18,4 +19,7 @@ export class VideoLabel {
 
     @ManyToOne(() => Video, (video) => video.id)
     video: Video
+
+    @ManyToOne(() => Assignment, (assignment) => assignment.id)
+    assignment: Assignment
 }
