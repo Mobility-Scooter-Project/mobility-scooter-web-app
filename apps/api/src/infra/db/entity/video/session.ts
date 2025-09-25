75d682b442970d1a807690abdf73a01ff9201653
Bryan <bryan.l.bergo@gmail.com>
2025-09-25 14:30:33 -0700

    feat: video migrations

---
+++ apps/api/src/infra/db/entity/video/session.ts
@@ -0,0 +1,20 @@
+import { SCHEMAS } from "@src/config/schemas";
+import { Column, Entity, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { CreateUpdateDeleteFields } from "../shared";
+import { Patient } from "../unit/patient";
+import { Unit } from "../unit/unit";
+
+@Entity({ schema: SCHEMAS.VIDEOS })
+export class PatientSession {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column(() => CreateUpdateDeleteFields)
+    cud: CreateUpdateDeleteFields
+
+    @OneToOne(() => Patient, (patient) => patient.id)
+    patient: Patient
+
+    @OneToOne(() => Unit, (unit) => unit.id)
+    unit: Unit
+}
---
+++ apps/api/src/infra/db/entity/video/video.ts
@@ -0,0 +1,20 @@
+import { SCHEMAS } from "@src/config/schemas";
+import { Column, Entity, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { CreateUpdateDeleteFields } from "../shared";
+import { PatientSession } from "./session";
+import { File } from "../unit/file";
+
+@Entity({ schema: SCHEMAS.VIDEOS })
+export class Video {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column(() => CreateUpdateDeleteFields)
+    cud: CreateUpdateDeleteFields
+
+    @ManyToOne(() => PatientSession, (session) => session.id)
+    session: PatientSession
+
+    @OneToOne(() => File, (file) => file.id)
+    file: File
+}
