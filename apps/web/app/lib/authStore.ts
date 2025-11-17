9a4246f77a2ed7e5ec9d17dddbe0514421b544bc
Jasper Liu <xizliu106@gmail.com>
2025-11-17 10:24:10 -0800

    refactor: added functions to refresh authStore tokens and loginAction error handling

--- apps/web/app/lib/authStore.ts
+++ apps/web/app/lib/authStore.ts
@@ -3,15 +3,21 @@ import { persist } from "zustand/middleware";
 
 type AuthType = {
   accessToken: string | null;
+  refreshToken: string | null;
   setAccessToken: (token: string | null) => void;
+  setRefreshToken: (token: string | null) => void;
   clearAccessToken: () => void;
+  clearRefreshToken: () => void;
 };
 export const userAuthStore = create<AuthType>()(
   persist(
     (set) => ({
       accessToken: null,
+      refreshToken: null,
       setAccessToken: (token) => set({ accessToken: token }),
+      setRefreshToken: (token) => set({ refreshToken: token }),
       clearAccessToken: () => set({ accessToken: null }),
+      clearRefreshToken: () => set({ refreshToken: null }),
     }),
     {
       name: "auth-storage", // key name to be saved in localStorage
