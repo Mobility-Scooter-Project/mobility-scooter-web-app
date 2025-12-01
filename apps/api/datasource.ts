0683c4a1dc1a030c7d1a5ef1f6cb47e8ef01b5c7
Bryan <bryan.l.bergo@gmail.com>
2025-11-30 20:02:23 -0800

    fix: use conn string for migrations on cluster

--- apps/api/datasource.ts
+++ apps/api/datasource.ts
@@ -3,14 +3,28 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export default new DataSource({
-    type: 'postgres',
-    host: process.env.DATABASE_HOST,
-    port: Number(process.env.DATABASE_PORT),
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    synchronize: false, // if this is set to true, updates will run automatically, and generating migrations will become impossible
-    migrations: ['./src/infra/db/migrations/*.{ts,js}'],
-    entities: ['./src/infra/db/entity/**/*.{ts,js}']
-})
+let datasource;
+
+if (process.env.DATABASE_URL) {
+    datasource = new DataSource({
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        synchronize: false,
+        migrations: ['./src/infra/db/migrations/*.{ts,js}'],
+        entities: ['./src/infra/db/entity/**/*.{ts,js}']
+    })
+} else {
+    datasource = new DataSource({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_PORT),
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        synchronize: false, // if this is set to true, updates will run automatically, and generating migrations will become impossible
+        migrations: ['./src/infra/db/migrations/*.{ts,js}'],
+        entities: ['./src/infra/db/entity/**/*.{ts,js}']
+    })
+}
+
+export default datasource;
