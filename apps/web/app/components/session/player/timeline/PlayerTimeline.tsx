1b1cf9e0e1a15b6184a30f4db0045efb1bddb7df
PattyCakesxD <nguyenpatty42@gmail.com>
2026-03-21 14:34:33 -0700

    refactor: timeline playhead line

--- apps/web/app/components/session/player/timeline/PlayerTimeline.tsx
+++ apps/web/app/components/session/player/timeline/PlayerTimeline.tsx
@@ -1,4 +1,5 @@
 import type { View } from "~/data/mock-session-data";
+import { TimelinePlayhead } from "./TimelinePlayhead";
 import { TimelineTicks } from "./TimelineTicks";
 import { AnnotationTrack } from "./tracks/AnnotationTrack";
 import { RiskTrack } from "./tracks/RiskTrack";
@@ -12,13 +13,21 @@ interface PlayerTimelineProps {
 
 export function PlayerTimeline({ activeView }: PlayerTimelineProps) {
   const duration = useVideoStore((s) => s.duration);
+  const currentTime = useVideoStore((s) => s.currentTime);
+  
+  const safeDuration = duration > 0 ? duration : 1;
+  const progressPercent = Math.min(Math.max((currentTime / safeDuration) * 100, 0), 100);
 
   return (
     <div className="flex flex-col gap-y-3">
-      <TimelineTicks duration={duration} />
-      <AnnotationTrack />
-      <RiskTrack activeView={activeView} duration={duration} />
-      <ChapterTrack />
+      <div className="relative flex flex-col gap-y-3 w-full">
+        <TimelinePlayhead />
+        <TimelineTicks duration={duration} />
+        <AnnotationTrack />
+        <RiskTrack activeView={activeView} duration={duration} />
+        <ChapterTrack />
+      </div>
+
       <PlayerControls />
     </div>
   );
