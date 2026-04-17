b84a8332dac0f0ecd1d5c1b86e363284f735f22b
PattyCakesxD <nguyenpatty42@gmail.com>
2026-04-17 00:35:14 -0700

    fix: pypino "warn" bug

--- apps/video-worker/src/utils/logger.py
+++ apps/video-worker/src/utils/logger.py
@@ -4,5 +4,8 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
 logger = PyPino(name="video-worker")
 
+if not hasattr(logger, "warning") and hasattr(logger, "warn"):
+    logger.warning = logger.warn
+
 if ENVIRONMENT != 'production':
     logger.level("debug")
